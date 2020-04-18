e0755cebebd87153bd7b69388d222472f36088bd
Tornike Menabde <tokamenabde@gmail.com>
2020-04-18 22:42:43 +0400

    before dusk

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,7 @@ use App\Billing\PaymentGateway;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 use App\HashidsTicketCodeGenerator;
+use Laravel\Dusk\DuskServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,17 +27,21 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->bind(StripePaymentGateway::class,  function(){ 
-            return new StripePaymentGateway(config('services.stripe.key')); 
+
+        if ($this->app->environment('local', 'testing')) {
+            $this->app->register(DuskServiceProvider::class);
+        }
+        $this->app->bind(StripePaymentGateway::class,  function () {
+            return new StripePaymentGateway(config('services.stripe.key'));
         });
 
         $this->app->bind(PaymentGateway::class, StripePaymentGateway::class);
-        $this->app->bind(\App\OrderConfirmationNumberGenerator::class,\App\RandomOrderConfirmationNumberGenerator::class);
+        $this->app->bind(\App\OrderConfirmationNumberGenerator::class, \App\RandomOrderConfirmationNumberGenerator::class);
 
-        $this->app->bind(HashidsTicketCodeGenerator::class, function(){
+        $this->app->bind(HashidsTicketCodeGenerator::class, function () {
             return new HashidsTicketCodeGenerator(config('app.ticket_code_salt'));
         });
 
-        $this->app->bind(\App\TicketCodeGenerator::class,\App\HashidsTicketCodeGenerator::class);
+        $this->app->bind(\App\TicketCodeGenerator::class, \App\HashidsTicketCodeGenerator::class);
     }
 }
